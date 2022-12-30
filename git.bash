9507f838f88d1d36cbe525cd5cf62376ebe14bcb
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2022-12-30 21:41:17 +1300

    it seems this work-around is no longer needed

--- git.bash
+++ git.bash
@@ -17,17 +17,9 @@ function currentbranch()
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 }
 
-function precmd()
-{
-	case $BASH_COMMAND in $PROMPT_COMMAND) ;;
-	*) ;;
-	esac
-}
-
 function preprompt()
 {
 	PS1='\[${FAINT}\][ \# | \u | \h ]\[${RESET}\]\n \[${PURPLE}\]\[${BOLD}\]\w\[${GREEN}\]$(currentbranch)\[${RESET}\] \$ '
 }
 
-trap precmd DEBUG
 PROMPT_COMMAND=preprompt
