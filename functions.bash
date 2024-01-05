a6bfd8d6bd787061a30d23d532198c779233da61
mightbesimon <60593422+mightbesimon@users.noreply.github.com>
2024-01-05 22:11:55 +1300

    remove `show`, allow no arg for `onthisday`

--- functions.bash
+++ functions.bash
@@ -1,27 +1,12 @@
 #!/bin/bash
 
 ################################################################
-#######                 quality of life                  #######
-################################################################
-function show()
-{
-	tabs -4
-	cat $1
-	tabs
-}
-
-################################################################
 #######                      config                      #######
 ################################################################
-function editor()
-{
-	EDITOR=$1
-}
-
 function config()
 {
 	case $1 in
-		bash    ) $EDITOR ~/profile.bash/;;
+		bash    ) $EDITOR ~/profile.bash/ ~/.bash_profile;;
 		git     ) $EDITOR ~/.gitconfig ~/.gitignore;;
 		python  ) $EDITOR ~/.config/pycodestyle;;
 		flake8  ) $EDITOR ~/.flake8;;
@@ -35,13 +20,12 @@ function config()
 ################################################################
 function onthisday()
 {
-	# echo '╭─────────────╮'
-	# echo '│ On this day │'
-	# echo '╰─────────────╯'
-	echo On this day:
-	echo ============
-	grep $1 /usr/share/calendar/calendar.birthday
-	grep $1 /usr/share/calendar/calendar.computer
-	grep $1 /usr/share/calendar/calendar.history
+	day=${1:-$(date +%m/%d)}
+	echo '╭─────────────╮'
+	echo '│ On this day │'
+	echo '╰─────────────╯'
+	grep $day /usr/share/calendar/calendar.birthday
+	grep $day /usr/share/calendar/calendar.computer
+	grep $day /usr/share/calendar/calendar.history
 	echo
 }
